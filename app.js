2539cc2e8ea327bb2ab7c38d56990d554f81fde6
frandrew90 <frandrewjs@gmail.com>
2022-06-14 15:21:15 +0300

    made auth routes

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+// const router = require("./routes/auth_routes.js");
+
+const app = express();
+
+app.use("/api/auth", require("./routes/auth_routes"));
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoURI"), {
+      useNewUrlParser: true,
+      //   useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () =>
+      console.log(`App has been started on PORT ${PORT}...`)
+    );
+  } catch (error) {
+    console.log("server error", error.message);
+    process.exit(1);
+  }
+}
+
+start();
---
+++ routes/auth_routes.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const User = require("../models/User");
+const router = Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const candidate = await User.findOne({ email });
+
+    if (candidate) {
+      return res
+        .status(400)
+        .json({ message: `The User with email: ${email} already exists` });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = new User({ email, password: hashedPassword });
+
+    await user.save();
+
+    res.status(201).json({ message: "New user was created" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+router.post("/login", async (req, res) => {});
+
+module.exports = router;
