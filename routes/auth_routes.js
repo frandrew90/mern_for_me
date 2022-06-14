c3a1088417942d5fd69f6964f1acef1840610b88
frandrew90 <frandrewjs@gmail.com>
2022-06-14 16:25:58 +0300

    made registration method

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -4,29 +4,45 @@ const router = Router();
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
-router.post("/register", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  "/register",
+  [
+    check("email", "Incorrect email").isEmail(),
+    check("password", "Incorrect password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({ email });
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Incorrect registration values",
+        });
+      }
 
-    if (candidate) {
-      return res
-        .status(400)
-        .json({ message: `The User with email: ${email} already exists` });
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: `The User with email: ${email} already exists` });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: "New user was created" });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const user = new User({ email, password: hashedPassword });
-
-    await user.save();
-
-    res.status(201).json({ message: "New user was created" });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
   }
-});
+);
 
 router.post("/login", async (req, res) => {});
 
